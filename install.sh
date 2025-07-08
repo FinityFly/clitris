8424395556a79bab9d09a3e1196dffba7cb49a07
Daniel Lu <daniel1211.lu@gmail.com>
2025-07-08 13:30:48 -0400

    feat: update installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-make
-sudo mv clitris /usr/local/bin/
+set -e
+git clone https://github.com/FinityFly/clitris.git
+cd clitris
+sudo mv clitris "/usr/local/bin/clitris"
 echo "Installation complete. You can now run the game using 'clitris'."
